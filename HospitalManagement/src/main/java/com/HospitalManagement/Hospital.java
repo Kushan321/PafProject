e4232a94a482fa5998999d95cc6da882a7e82b74
MuRemo <munzirramo18@gmail.com>
2020-04-17 14:04:01 +0530

    Hospital management(Create,delete,view)

---
+++ HospitalManagement/src/main/java/com/HospitalManagement/Hospital.java
@@ -0,0 +1,43 @@
+package com.HospitalManagement;
+
+public class Hospital {
+
+	private String name;
+	private String address;
+	private String charge;
+	private String phonenumber;
+	private String roomcount;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCharge() {
+		return charge;
+	}
+	public void setCharge(String charge) {
+		this.charge = charge;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getRoomcount() {
+		return roomcount;
+	}
+	public void setRoomcount(String roomcount) {
+		this.roomcount = roomcount;
+	}
+	
+	
+	
+}
